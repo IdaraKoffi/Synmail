c4a640d70e22eddbd08edc7f148470d175432ffe
IdaraKoffi <idarakoffi@gmail.com>
2023-12-28 05:44:55 -0800

    calling backend

---
+++ email-synchronizer-frontend/httpClient.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const registerUser = async (userData) => {
+  try {
+    const response = await axios.post('http://localhost:${port}/register', userData);
+
+    // Axios automatically throws an error for non-2xx responses
+    const data = response.data;
+    console.log('Response from server:', data);
+
+    // Handle the response data or update the UI as needed
+  } catch (error) {
+    console.error('Error during Axios request:', error);
+    // Handle errors, update the UI accordingly
+  }
+};
--- login_page.js
+++ login_page.js
@@ -1,73 +1,72 @@
-//backend login script
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const bcrypt = require('bcrypt');
+const cors = require('cors'); // Add this line for CORS support
 const User = require('./models/User');
 
 const app = express();
 const port = 3000;
 
-// Connect to MondoDB
-mongoose.connect('mongodb://
+// Connect to MongoDB (Replace 'mongodb://...' with your actual MongoDB connection string)
+mongoose.connect('mongodb://localhost:${port}');
 
-//Middleware to parse JSON
+// Middleware to parse JSON
 app.use(bodyparser.json());
+
+// Enable CORS middleware
+app.use(cors());
 
 // User registration endpoint
 app.post('/register', async (req, res) => {
-	const { email, password } = req.body;
-	
-	// Check if the user already exists
-	const existinguser = await User.findOne({ email });
-	if (existinguser) {
-		return res.status(400).json({ success: false, message: 'User already exist' });
-	}
-	// Hash the password
-	const hashedpassword = await bcrpt.hash(password, 10);
+    try {
+        // Extract email and password from the request body
+        const { email, password } = req.body;
 
-	// Create a new user
-	const newuser = new User({
-		email,
-		password: hashedpassword,
-	});
-	// Save the user to the database
-	res.json({ success: true, message: 'Registration successful' });
-} catch (error) {
-	console.error('Error:' error);
-	res.status(500).json({ success: false, message: 'Registration error' });
-}
+        // Check if the user already exists in the database
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: 'User already exists' });
+        }
 
-//Login endpoint
-app.post('/login', (req, res) => {
-	const { username, password } = req.body;
+        // Hash the password using bcrypt
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-	// Find user by username
-	const user = users.find((user) => user.username === username);
-	// Checks if the user exists and the password is correct
-	if (user && user.password === password) {
-		res.json({ success: true, message: 'Login successful'});
-	} else {
-		res.status(401).json({ success: false, message: 'Invalid username or password' });
-	}
-	app.post('/login', async (req, res) => {
-		const { email, password } = req.body;
+        // Create a new user instance
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+        });
 
-		// Find user by email
-		const user = await User.findOne({ email });
+        // Save the new user to the database
+        await newUser.save();
 
-		// Check if user exists and the password is correct
-		if (user && await bcrypt.compare(password, user.password)) {
-			res.json({ success: true, message: 'Login Successfull' });
-	} else {
-		res.status(401).json({ success: false, message: 'Invalid email or password' });
-	}
-	} catch (error) {
-		console.error('Error:', error);
-		res.status(500).json({ success: false, message: 'Error during login'});
-	}
+        res.json({ success: true, message: 'Registration successful' });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ success: false, message: 'Registration error' });
+    }
+});
 
+// Login endpoint
+app.post('/login', async (req, res) => {
+    try {
+        // Extract email and password from the request body
+        const { email, password } = req.body;
+
+        // Find the user in the database by email
+        const user = await User.findOne({ email });
+
+        // Check if the user exists and the password is correct
+        if (user && await bcrypt.compare(password, user.password)) {
+            res.json({ success: true, message: 'Login successful' });
+        } else {
+            res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ success: false, message: 'Error during login' });
+    }
 });
 
 // Start the server
